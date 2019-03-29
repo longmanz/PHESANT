d918ad629b89c6bd2ef0d5826a1c2e6b7cab23c7
astheeggeggs <dpalmer@broadinstitute.org>
2019-03-29 14:58:39 -0400

    fixed closed paren in options list

--- WAS/phenomeScan.r
+++ WAS/phenomeScan.r
@@ -56,8 +56,8 @@ option_list <- list(
   make_option(c("-m", "--mincategorysize"), type="integer", default=10,
     help="The minimum number of samples in a category for categorical single, integer, and continous variables."),
   make_option(c("-e", "--maxunorderedcategories"), type="integer", default=1000,
-    help="The maximum number of categories in an unordered categorical variable"),
-  make_option(c("-g", "--propforcontinuous", type="double", default=0.2,
+    help="The maximum number of categories in an unordered categorical variable."),
+  make_option(c("-g", "--propforcontinuous"), type="double", default=0.2,
     help="The cutoff for proportion of samples with the same value for the variable to not be considered continuous."),
   make_option(c("-i", "--inttocontcutoff", type="integer", default=10,
     help="The cutoff for the number of distinct integer values to send an integer variable to a continuous variable."))
