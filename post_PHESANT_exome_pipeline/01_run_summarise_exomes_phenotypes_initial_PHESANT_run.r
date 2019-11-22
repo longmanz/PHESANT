409bca09bd42e6edd48f78fe2e1c29de2f6642a6
astheeggeggs <duncan.stuart.palmer@gmail.com>
2019-11-22 15:05:27 -0500

    added new outcome info file

--- post_PHESANT_exome_pipeline/01_run_summarise_exomes_phenotypes_initial_PHESANT_run.r
+++ post_PHESANT_exome_pipeline/01_run_summarise_exomes_phenotypes_initial_PHESANT_run.r
@@ -4,18 +4,17 @@ source("../summarise_phenotypes.r")
 # Biomarkers for first 100K run
 # n_chunks <- 1
 # filename_root <- "biomarkers/pharma_exomes_biomarkers_parsed_output_100k_chunk."
-# phenotype_info_file <- "variable-info/outcome_info_final_round3.tsv"
+# variable_info <- "variable-info/outcome_info_final_round3.tsv"
 # coding_info_file <- "variable-info/data-coding-ordinal-info.txt"
 
 # Note: coding info file for the first 100K run was the following file, rather than the recently updated file referred to below.
 # coding_info_file <- "variable-info/data-coding-ordinal-info.txt"
 
-n_exomes <- '200K'
+n_exomes <- '200k'
 n_chunks <- 10
 
-outdir <- '../../'
-variable_info <- "../variable-info/outcome_info_final_pharma.tsv"
-filename_root <- paste0("pharma_exomes_parsed_output_", n_exomes, "_chunk.")
+variable_info <- "../variable-info/outcome_info_final_pharma_nov2019.tsv"
+filename_root <- paste0("../../pharma_exomes_parsed_output_", n_exomes, "_chunk.")
 coding_info_file <- "../variable-info/data-coding-ordinal-info-nov2019-update.txt"
 
 # Read in all the data table codings
@@ -34,13 +33,13 @@ for (i in 1:n_chunks)
 	pheno_summary <- paste0(filename_root, i, "_phenosummary.tsv")
 
 	filename <- paste0(filename_root, i)
-	tsv_filename <- paste(filename, ".tsv", sep="")
+	tsv_filename <- paste0(filename, ".tsv")
 	log_filename <- paste0(filename_root, i)
-	log_file <- paste(log_filename, ".log", sep="")
+	log_file <- paste0(log_filename, ".log")
 	tsv_data <- read.table(tsv_filename, header=TRUE, sep='\t')
 	names(tsv_data)[1] <- "userId"
 
-	outcome_info <- read.table(phenotype_info_file, sep='\t', quote="", comment.char="", header=TRUE)
+	outcome_info <- read.table(variable_info, sep='\t', quote="", comment.char="", header=TRUE)
 	print(paste0("both sexes ", i))
 	summary_file <-  get_hists_and_notes(hist_filename, tsv_data, log_file, outcome_info, codings_tables, samples_for_inclusion=TRUE, check=FALSE, start_column=4)
 	write.table(summary_file, file=pheno_summary, col.names=TRUE, row.names=TRUE, sep='\t', quote=FALSE)
