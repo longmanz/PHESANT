4986e43dad3fea162d1eb6db96ebf1371cc8a85a
astheeggeggs <duncan.stuart.palmer@gmail.com>
2019-11-22 18:01:19 -0500

    altered the binary encoding to always send binary variables to TRUE/FALSE

--- WAS/binaryLogisticRegression.r
+++ WAS/binaryLogisticRegression.r
@@ -1,6 +1,5 @@
 # Performs binary logistic regression on the phenotype stored in this data 
 # and stores result in 'results-logistic-binary' results file.
-
 binaryLogisticRegression <- function(varName, varType, thisdata, varlogfile)
 {   
     to_save <- thisdata[, phenoStartIdx]
@@ -14,16 +13,25 @@ binaryLogisticRegression <- function(varName, varType, thisdata, varlogfile)
         incrementCounter("binary.nottwolevels")
     }
 
-    idxTrue <- length(which(phenoFactor == facLevels[1]))
-    idxFalse <- length(which(phenoFactor == facLevels[2]))
+    idxTrue <- which(phenoFactor == facLevels[2])
+    idxFalse <- which(phenoFactor == facLevels[1])
     numNotNA <- length(which(!is.na(phenoFactor)))
+
+    # Added to ensure that TRUE/FALSE is returned for these binary variables.
+    phenoFactor_tmp <- rep.int(NA, length(phenoFactor))
+    phenoFactor_tmp[idxTrue] <- TRUE
+    phenoFactor_tmp[idxFalse] <- FALSE
+    phenoFactor <- factor(phenoFactor)
+
+    idxTrue <- length(idxTrue)
+    idxFalse <- length(idxFalse)
 
     if (idxTrue < opt$bintruecutoff || idxFalse < opt$bintruecutoff) {
         cat("BINARY-LOGISTIC-SKIP-", opt$bintruecutoff, " (", idxTrue, "/", idxFalse, ") || ",
             sep="", file=varlogfile, append=TRUE)
         incrementCounter(paste("binary.", opt$bintruecutoff, sep=""))
         return(NULL)
-    } else if (numNotNA < 500) {	
+    } else if (numNotNA < 500) {    
         cat("BINARY-LOGISTIC-SKIP-", opt$binnacutoff, " (", numNotNA, ") || ",
             sep="", file=varlogfile, append=TRUE)
         incrementCounter(paste("binary.", opt$binnacutoff, sep=""))
@@ -44,4 +52,3 @@ binaryLogisticRegression <- function(varName, varType, thisdata, varlogfile)
         })
     }
 }
-
